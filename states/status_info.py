49a8decba0c8c1c7e5d05b97ec275cbbe0bd4d0f
Maria Orlova <vampusha07@gmail.com>
2022-10-01 00:06:44 +0300

    корректировка состояний HotelStatus(StatesGroup)

--- states/status_info.py
+++ states/status_info.py
@@ -1,42 +1,17 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class BestdealStatus(StatesGroup):
+class HotelStatus(StatesGroup):
     city = State()
     check_in = State()
     check_out = State()
     min_price = State()
     max_price = State()
-    min_distance_center = State()
-    max_distance_center = State()
+    min_distance = State()
+    max_distance = State()
     hotels_count = State()
     get_photo = State()
     photo_count = State()
-    executing_request = State()
-
-
-class LowpriceStatus(StatesGroup):
-    city = State()
-    check_in = State()
-    check_out = State()
-    min_price = State()
-    max_price = State()
-    hotels_count = State()
-    get_photo = State()
-    photo_count = State()
-    executing_request = State()
-
-
-class HighpriceStatus(StatesGroup):
-    city = State()
-    check_in = State()
-    check_out = State()
-    min_price = State()
-    max_price = State()
-    hotels_count = State()
-    get_photo = State()
-    photo_count = State()
-    executing_request = State()
 
 
 # MAX_HOTELS = 25
