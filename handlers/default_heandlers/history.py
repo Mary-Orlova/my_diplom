4dd2eaf446ce2399e4e0df8bf759425d73339ccb
Maria Orlova <vampusha07@gmail.com>
2022-10-26 20:17:19 +0300

    корректировка файла history в handlers/default_heandlers

--- handlers/default_heandlers/history.py
+++ handlers/default_heandlers/history.py
@@ -1,20 +1,16 @@
-# import json
 # import sqlite3
-# import codecs
+from states.status_info import HotelStatus
 from aiogram.types import Message
-from loader import dp
+# from loader import dp
 # from database import base as db_hotels
 from aiogram import Dispatcher
-from aiogram.dispatcher import FSMContext
 from loguru import logger
-# from datetime import datetime
-# from django.db import DatabaseError, transaction
-# from peewee import PrimaryKeyField, IntegerField, CharField, DateTimeField
-#
-# # @dp.message_handler(commands=["history"])
-# # async def history(message: Message, state: FSMContext):
-#
-#
+from datetime import datetime
+# import requests, json
+
+
+# @dp.message_handler(commands=["history"])
+# async def history(message: Message, state: FSMContext):
 @logger.catch()
 async def history(message: Message):
     logger.info('Команда /history была вызвана.')
@@ -25,37 +21,14 @@ async def history(message: Message):
 def register_handlers_history(dp: Dispatcher):
     dp.register_message_handler(history, commands=['history'])
 
-#
-# class Requests(BaseModel):
-#     """Класс хранения истории пользователя"""
-#     id = PrimaryKeyField(null=False)
-#     user_id = IntegerField()
-#     command = CharField()
-#     city_name = IntegerField()
-#     created_at = DateTimeField(default=datetime.now())
-#
-#     class Meta:
-#         db_hotels = 'requests'
-#
-# def add_request(parameters: dict):
-#     """Внесение данных запроса пользователя в таблицу"""
-#     with db_hotels.atomic():
-#         try:
-#             row = Requests(user_id=parameters.get('user_id'),
-#                            command=parameters.get('command'),
-#                            city_name=parameters.get('city_name'),
-#                            city_id=parameters.get('city_id'),
-#                            hotels=json.dump(parameters.get('hotels'), ensure_ascii=False).encode('utf-8'))
-#             row.save()
-#         except:
-#             logger.exception('Произошла ошибка-исключение в add_request')
-#
-#
-# def get_user_history(user_id):
-#     """Выаолнения запроса по истории пользователя - запрос в БД"""
-#     with db_hotels:
-#         try:
-#             user_history = Requests.select().where(Requests.user_id == user_id)
-#             return True, user_history
-#         except DatabaseError:
-#             return False, None
+
+class Requests():
+    def __init__(self, user_id, command, city_name=HotelStatus.city_name, city_id=HotelStatus.city):
+        self.id = user_id
+        self.command = command
+        self.city_name = city_name
+        self.city_id = city_id
+        self.created_at = datetime.now
+
+        class Meta:
+            db_hotels = 'requests'
