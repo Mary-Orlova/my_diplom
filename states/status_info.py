fc1d7866c44b5bcf51b76e3037fe8b52a563a4bc
Maria Orlova <vampusha07@gmail.com>
2022-10-26 20:18:37 +0300

    корректировка файла states_info в states (добавление city_name)

--- states/status_info.py
+++ states/status_info.py
@@ -2,6 +2,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class HotelStatus(StatesGroup):
+    order = State()
+    city_name = State()
     city = State()
     check_in = State()
     check_out = State()
