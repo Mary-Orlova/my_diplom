08fa285128e87ae955629278401afa061dcd8390
Maria Orlova <vampusha07@gmail.com>
2022-11-03 19:40:36 +0300

    добавлена регистрация команды lowprice (сценарий работы команды частично идет сценарию по bestdeal.py)

--- handlers/default_heandlers/lowprice.py
+++ handlers/default_heandlers/lowprice.py
@@ -1,32 +1,15 @@
-from aiogram.types import Message
-from aiogram import Dispatcher
-from aiogram.dispatcher import FSMContext
-from aiogram.utils.callback_data import CallbackData
-# from states.status_info import LowpriceStatus
-# from information_API import hotels
-# from loader import dp
-# from config_data.config import DEFAULT_COMMANDS
-from loguru import logger
-# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-# from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
-
-# @dp.message_handler(commands=["lowprice"])
-# async def lowprice(message: Message, state: FSMContext) -> None:
-
-lowhighprice_callback_data = CallbackData('lowhighprice', 'action', 'city_id')
+from states.status_info import HotelStatus
+from handlers.default_heandlers import bestdeal
 
 
-@logger.catch()
-async def lowprice(message: Message, state: FSMContext) -> None:
-    await state.finish()
-    # chat_id = message.chat.id
-    # redis_db.hset(chat_id, 'state', 1)
-    # redis_db.hset(chat_id, 'order', 'PRICE')
-    logger.info('Команда "/lowprice" была вызвана.')
-    # logging.info(redis_db.hget(chat_id, 'order'))
-    # state = redis_db.hget(chat_id, 'state')
-    await message.answer('команда поиска дешевых отелей была вызвана')
-
-
-def register_handlers_lowprice(dp: Dispatcher):
-    dp.register_message_handler(lowprice, commands=['lowprice'], state='*')
+def register_handlers_lowprice(dp):
+    dp.register_message_handler(bestdeal.command_handler,
+                                commands=['lowprice', 'highprice', 'bestdeal'], state='*')
+    dp.register_message_handler(bestdeal.city_name_handler, state=HotelStatus.city)
+    dp.register_callback_query_handler(bestdeal.keyboard_handler, state=HotelStatus.city)
+    dp.register_callback_query_handler(bestdeal.callback_calendar, state=HotelStatus.check_in)
+    dp.register_callback_query_handler(bestdeal.callback_check_out, state=HotelStatus.check_out)
+    dp.register_message_handler(bestdeal.hotels_count_handler, state=HotelStatus.hotels_count)
+    dp.register_message_handler(bestdeal.get_photo_handler, state=HotelStatus.get_photo)
+    dp.register_message_handler(bestdeal.photo_count_handler, state=HotelStatus.photo_count)
+    dp.register_message_handler(bestdeal.get_requests, state=HotelStatus.photo_count)
