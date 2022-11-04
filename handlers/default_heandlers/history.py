b7416b709ffa20041ce087ada16dd1cf7c3d21a5
Maria Orlova <vampusha07@gmail.com>
2022-11-05 00:30:37 +0300

    корректировка файла history

--- handlers/default_heandlers/history.py
+++ handlers/default_heandlers/history.py
@@ -1,34 +1,29 @@
-# import sqlite3
-from states.status_info import HotelStatus
 from aiogram.types import Message
-# from loader import dp
-# from database import base as db_hotels
+from database import base as db_hotels
 from aiogram import Dispatcher
 from loguru import logger
-from datetime import datetime
-# import requests, json
-
-
-# @dp.message_handler(commands=["history"])
-# async def history(message: Message, state: FSMContext):
-@logger.catch()
-async def history(message: Message):
-    logger.info('Команда /history была вызвана.')
-    await message.answer('вызвана команда истории')
 
 
 @logger.catch()
 def register_handlers_history(dp: Dispatcher):
     dp.register_message_handler(history, commands=['history'])
+    dp.register_message_handler(history, commands=['delete_history'])
 
 
-class Requests():
-    def __init__(self, user_id, command, city_name=HotelStatus.city_name, city_id=HotelStatus.city):
-        self.id = user_id
-        self.command = command
-        self.city_name = city_name
-        self.city_id = city_id
-        self.created_at = datetime.now
+@logger.catch()
+async def history(message: Message) -> None:
+    """ Метод вывода истории из БД пользователю в телеграм-бот.
+    :param message: сообщение пользователя."""
+    logger.info(f'Команда /history была вызвана, id = {message.from_user.id}')
+    user_id = int(message.from_user.id)
+    limit = '10'
+    db_hotels.show_history(user_id, limit)
 
-        class Meta:
-            db_hotels = 'requests'
+
+@logger.catch()
+async def delete_history(message: Message) -> None:
+    """ Метод удаления истории из БД.
+    :param message: сообщение пользователя."""
+    logger.info(f'Команда /delete_history была вызвана, id = {message.from_user.id}')
+    id_string = int(message.from_user.id)
+    db_hotels.delete_history(id_string)
