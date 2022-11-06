5ed4e1acfcabf7ee7d37ec1520f26d226634e271
Maria Orlova <vampusha07@gmail.com>
2022-11-07 01:40:56 +0300

    доработана команда по работе с историей пользователя

--- handlers/default_heandlers/history.py
+++ handlers/default_heandlers/history.py
@@ -2,12 +2,13 @@ from aiogram.types import Message
 from database import base as db_hotels
 from aiogram import Dispatcher
 from loguru import logger
+from loader import bot
 
 
 @logger.catch()
 def register_handlers_history(dp: Dispatcher):
     dp.register_message_handler(history, commands=['history'])
-    dp.register_message_handler(history, commands=['delete_history'])
+    dp.register_message_handler(delete_history, commands=['delete_history'])
 
 
 @logger.catch()
@@ -16,9 +17,18 @@ async def history(message: Message) -> None:
     :param message: сообщение пользователя."""
     logger.info(f'Команда /history была вызвана, id = {message.from_user.id}')
     user_id = int(message.from_user.id)
-    limit = '10'
-    history_info = db_hotels.show_history(user_id, limit)
-    await message.answer(str(history_info))
+    history_info = db_hotels.show_history(user_id)
+
+    if history_info:
+        for note in history_info:
+            await bot.send_message(message.chat.id,
+                                   f'Время: {note[0]}'
+                                   f'\nКоманда: {note[1]}'
+                                   f'\nМесто поиска: {note[2]}'
+                                   f'\nИнформация по отелю: {note[3]}'
+                                   )
+    else:
+        await bot.send_message(message.chat.id, 'Нет сохраненных результатов.')
 
 
 @logger.catch()
@@ -28,3 +38,4 @@ async def delete_history(message: Message) -> None:
     logger.info(f'Команда /delete_history была вызвана, id = {message.from_user.id}')
     id_string = int(message.from_user.id)
     db_hotels.delete_history(id_string)
+    await bot.send_message(message.chat.id, 'Пользователь удалил историю.')
