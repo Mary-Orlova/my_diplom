58ee5f1f6afd010abbc2a0b26d50703f15b61b6c
Мария Орлова <vampusha07@gmail.com>
2022-11-06 22:45:45 +0000

    Delete calendar.py

--- keyboards/inline/calendar.py
+++
@@ -1,6 +0,0 @@
-from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
-from datetime import date
-
-today = date.today()
-calendar, step = DetailedTelegramCalendar(min_date=today).build()
-step = LSTEP[step]
